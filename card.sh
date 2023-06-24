95ff17fa495561b8bdb56860b0aee0bff622fbe9
drmalik7 <uchmandx2@gmail.com>
2023-06-24 13:29:33 +0100

    card commit

---
+++ card.sh
@@ -0,0 +1,9 @@
+echo Secure details only pleaase
+echo Please input username
+read name
+echo verifying credentials please wait
+sleep 5
+echo Please enter passcode
+read -s code
+echo $name your passcode is verified
+echo Welcome, how can we help
