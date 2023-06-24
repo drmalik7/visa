f1ddd415723987e67fddfbc15f415210822316b5
drmalik7 <uchmandx2@gmail.com>
2023-06-24 14:53:06 +0100

    card commit 5

--- card.sh
+++ card.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo Secure details only pleaase
 echo Please input username
 read name
@@ -8,3 +9,9 @@ read -s code
 echo $name your passcode is verified
 echo Welcome, how can we help you
 echo Help is guaranteed
+echo what city are you from?
+read city
+echo your $city has been located $name
+sleep 5
+echo $name please meet the supervisor for $city
+
